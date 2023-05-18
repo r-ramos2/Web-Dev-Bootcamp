ede09259e814c24013f09bff95874db7d5c6f846
R <90172092+ocramos2@users.noreply.github.com>
2023-05-18 16:06:17 -0600

    Add files via upload

---
+++ 00 Superheroes nodejs/index.js
@@ -0,0 +1,10 @@
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+var supervillains = require("superheroes");
+
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainName);
